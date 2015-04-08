63719c01031a50fb96f8f8a4bbaa87a6020a4ea1
Elias Nygren <elias.nygren@cs.helsinki.fi>
2015-04-08 13:17:38 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup
 
 setup(name='UpCloud',
       version='0.1',
@@ -8,5 +9,13 @@ setup(name='UpCloud',
       author='Elias Nygren',
       author_email='elias.nygren@upcloud.com',
       url='https://www.upcloud.com',
-      packages=['upcloud'],
-     )
+      packages=['upcloud', 'upcloud.cloud_manager'],
+      install_requires=[
+  		'mock==1.0.1',
+		'py==1.4.26',
+		'pytest==2.6.4',
+		'requests==2.6.0',
+		'responses==0.3.0',
+		'six==1.9.0'
+      ]
+)
