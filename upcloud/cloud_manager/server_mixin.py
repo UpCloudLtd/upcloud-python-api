817891237988a793a004bd58ff5239fd7ac924e6
Elias Nygren <elias.nygren@upcloud.com>
2015-07-09 11:38:19 +0300

    add support for creating server with a dict instead of Server object

--- upcloud/cloud_manager/server_mixin.py
+++ upcloud/cloud_manager/server_mixin.py
@@ -2,17 +2,15 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
-from builtins import dict
-from builtins import str
+from builtins import dict, str, object
+
 from future import standard_library
 standard_library.install_aliases()
-from builtins import object
-from ..ip_address import IP_address
-from ..storage import Storage
 
-from ..server import Server
-
-from ..tools import assignIfExists
+from upcloud.tools import assignIfExists
+from upcloud import IP_address
+from upcloud import Storage
+from upcloud import Server
 
 
 class ServerManager(object):
@@ -60,6 +58,9 @@ class ServerManager(object):
 		Creates a server and its storages based on a (locally created) Server object.
 		Populates the given Server instance with the API response.
 
+		0.3.0: also supports giving the entire POST body as a dict that is directly
+		serialised into JSON. Refer to the REST API documentation for correct format.
+
 		Example:
 		server1 = Server( core_number = 1,
 					memory_amount = 512,
@@ -84,8 +85,20 @@ class ServerManager(object):
 			"Windows 2003","Windows 2008" ,"Windows 2012"
 
 		"""
+		if isinstance(server, Server):
+			body = server.prepare_post_body()
+		else:
+			ip_data = server.pop("ip_addresses", None)
+			storage_data = server.pop("storage_devices", None)
 
-		body = server.prepare_post_body()
+			server_dict = dict()
+			server_dict['cloud_manager'] = self
+			if ip_data: 	 server_dict['ip_addresses'] = IP_address._create_ip_address_objs( ip_data, cloud_manager = self )
+			if storage_data: server_dict['storage_devices'] = Storage._create_storage_objs( storage_data, cloud_manager = self )
+
+			server_from_dict = Server(server, **server_dict)
+			body = server_from_dict.prepare_post_body()
+
 
 		res = self.post_request("/server", body)
 
@@ -93,12 +106,17 @@ class ServerManager(object):
 		IP_addresses = IP_address._create_ip_address_objs( res["server"].pop("ip_addresses"), cloud_manager = self )
 		storages = Storage._create_storage_objs( res["server"].pop("storage_devices"), cloud_manager = self )
 
-		server._reset( res["server"],
-					ip_addresses = IP_addresses,
-					storage_devices = storages,
-					cloud_manager = self,
-					populated = True)
-		return server
+		if isinstance(server, Server):
+			server_to_return = server
+		else:
+			server_to_return = server_from_dict
+
+		server_to_return._reset( res["server"],
+						ip_addresses = IP_addresses,
+						storage_devices = storages,
+						cloud_manager = self,
+						populated = True)
+		return server_to_return
 
 
 	def modify_server(self, UUID, **kwargs):
