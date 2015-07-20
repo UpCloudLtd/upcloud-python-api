589ada0163d4068230445b3aa54e31ed35fea51d
Elias Nygren <elias.nygren@upcloud.com>
2015-07-20 15:28:32 +0300

    bump version to 3.0.1

--- upcloud_api/__init__.py
+++ upcloud_api/__init__.py
@@ -9,16 +9,15 @@ from __future__ import absolute_import
 from future import standard_library
 standard_library.install_aliases()
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __author__ = "Elias Nygren"
 __author_email__ = "elias.nygren@upcloud.com"
 __license__ = "MIT"
 __copyright__ = "Copyright (c) 2015 Elias Nygren"
 
-
-from upcloud_api.server import Server
 from upcloud_api.storage import Storage
 from upcloud_api.ip_address import IP_address
+from upcloud_api.server import Server
 from upcloud_api.firewall import FirewallRule
 from upcloud_api.tools import OperatingSystems, ZONE
 from upcloud_api.tag import Tag
