48c68fe7eb3313cbfd48d6d75c7da1e910ed1575
Elias Nygren <elias.nygren@upcloud.com>
2016-05-11 14:15:00 +0300

    update template shortcuts

--- upcloud_api/tools.py
+++ upcloud_api/tools.py
@@ -21,20 +21,22 @@ class ZONE(object):
 	"""
 	Enums for UpCloud's Zones.
 	"""
-	Helsinki = "fi-hel1"
-	London = "uk-lon1"
-	Chicago = "us-chi1"
+	Helsinki = 'fi-hel1'
+	London = 'uk-lon1'
+	Chicago = 'us-chi1'
+	Frankfurt = 'de-fra1'
 
 class OperatingSystems(object):
 	templates = {
-		"CentOS 6.5":	"01000000-0000-4000-8000-000050010200",
-		"CentOS 7.0":	"01000000-0000-4000-8000-000050010300",
-		"Debian 7.8":	"01000000-0000-4000-8000-000020020100",
-		"Ubuntu 12.04":	"01000000-0000-4000-8000-000030030200",
-		"Ubuntu 14.04":	"01000000-0000-4000-8000-000030040200",
-		"Windows 2003":	"01000000-0000-4000-8000-000010040400",
-		"Windows 2008":	"01000000-0000-4000-8000-000010030200",
-		"Windows 2012":	"01000000-0000-4000-8000-000010050200"
+		'CentOS 6.5':	'01000000-0000-4000-8000-000050010200',
+		'CentOS 7.0':	'01000000-0000-4000-8000-000050010300',
+		'Debian 7.8':	'01000000-0000-4000-8000-000020020100',
+		'Debian 8.0':	'01000000-0000-4000-8000-000020030100',
+		'Ubuntu 12.04':	'01000000-0000-4000-8000-000030030200',
+		'Ubuntu 14.04':	'01000000-0000-4000-8000-000030040200',
+		'Ubuntu 16.04': '01000000-0000-4000-8000-000030060200',
+		'Windows 2008':	'01000000-0000-4000-8000-000010030200',
+		'Windows 2012':	'01000000-0000-4000-8000-000010050200'
 	}
 
 	@classmethod
@@ -51,6 +53,8 @@ class OperatingSystems(object):
 		if re.search(uuid_regexp, os):
 			return os
 
-		raise Exception('Invalid OS -- valid options are: "CentOS 6.5", "CentOS 7.0"',
-						'"Debian 7.8", "Ubuntu 12.04", "Ubuntu 14.04", "Windows 2003"',
-						'"Windows 2008", "Windows 2012"')
+		raise Exception((
+			"Invalid OS -- valid options are: 'CentOS 6.5', 'CentOS 7.0', "
+			"'Debian 7.8', 'Debian 8.0' ,'Ubuntu 12.04', 'Ubuntu 14.04', 'Ubuntu 16.04', "
+			"'Windows 2008', 'Windows 2012'"
+		))
