cc12a840781b4e48e9ad285eaee2eebb5ec6369a
Elias Nygren <elias.nygren@upcloud.com>
2016-07-18 13:47:41 +0300

    bump version to 0.3.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='upcloud-api',
-    version='0.3.5',
+    version='0.3.6',
     description='UpCloud API Client',
     author='Elias Nygren',
     author_email='elias.nygren@upcloud.com',
@@ -12,14 +12,10 @@ setup(
     maintainer_email='elias.nygren@upcloud.com',
     url='https://github.com/UpCloudLtd/upcloud-python-api',
     packages=['upcloud_api', 'upcloud_api.cloud_manager'],
-    download='https://github.com/UpCloudLtd/upcloud-python-api/tarball/v0.3.5',
+    download='https://github.com/UpCloudLtd/upcloud-python-api/tarball/v0.3.6',
     license='MIT',
     install_requires=[
-    	'future==0.14.3',
-        'mock==1.0.1',
-        'requests==2.6.0',
-        'responses==0.3.0',
-        'six==1.9.0',
-        'wheel==0.24.0'
+        'requests>=2.6.0',
+        'six>=1.9.0'
     ]
 )
