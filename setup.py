091fdbeb70704c1dcc681bc7e04516c38bcb1617
Mika Lackman <mika.lackman@upcloud.com>
2018-02-02 06:50:27 +0200

    setup updated

--- setup.py
+++ setup.py
@@ -4,15 +4,14 @@ from setuptools import setup
 
 setup(
     name='upcloud-api',
-    version='0.3.9',
+    version='0.4.1',
     description='UpCloud API Client',
     author='Elias Nygren',
-    author_email='elias.nygren@upcloud.com',
-    maintainer='Elias Nygren',
-    maintainer_email='elias.nygren@upcloud.com',
+    maintainer='Mika Lackman',
+    maintainer_email='mika.lackman@upcloud.com',
     url='https://github.com/UpCloudLtd/upcloud-python-api',
     packages=['upcloud_api', 'upcloud_api.cloud_manager'],
-    download='https://github.com/UpCloudLtd/upcloud-python-api/tarball/v0.3.9',
+    download='https://github.com/UpCloudLtd/upcloud-python-api/tarball/v0.4.1',
     license='MIT',
     install_requires=[
         'requests>=2.6.0',
