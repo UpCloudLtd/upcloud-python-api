954b0ad7c4b932b2be31a95d88975f6b0eeac8ed
Codeslayer666 <samir.alkurdi@upcloud.com>
2018-12-03 13:33:51 +0200

    update setup file with new version

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
     name='upcloud-api',
-    version='0.4.2',
+    version='0.4.3',
     description='UpCloud API Client',
     author='Elias Nygren',
     maintainer='Mika Lackman',
     maintainer_email='mika.lackman@upcloud.com',
     url='https://github.com/UpCloudLtd/upcloud-python-api',
     packages=['upcloud_api', 'upcloud_api.cloud_manager'],
-    download='https://github.com/UpCloudLtd/upcloud-python-api/tarball/v0.4.1',
+    download='https://github.com/UpCloudLtd/upcloud-python-api/tarball/v0.4.3',
     license='MIT',
     install_requires=[
         'requests>=2.6.0',
