eccc88e8fc336b18348b3f7369a538fcc7d07c1a
Codeslayer666 <samir.alkurdi@upcloud.com>
2019-12-03 10:16:04 +0200

    Correct parameter name is download_url.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     maintainer_email='mika.lackman@upcloud.com',
     url='https://github.com/UpCloudLtd/upcloud-python-api',
     packages=['upcloud_api', 'upcloud_api.cloud_manager'],
-    download='https://github.com/UpCloudLtd/upcloud-python-api/archive/0.4.5.tar.gz',
+    download_url='https://github.com/UpCloudLtd/upcloud-python-api/archive/0.4.5.tar.gz',
     license='MIT',
     install_requires=[
         'requests>=2.6.0',
