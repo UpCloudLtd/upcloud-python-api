bf96f4915e119527dc805853c4b742525ceee705
Ville Skyttä <ville.skytta@upcloud.com>
2019-12-12 17:26:30 +0200

    setup.py: add python_requires (#57)

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     packages=['upcloud_api', 'upcloud_api.cloud_manager'],
     download_url='https://github.com/UpCloudLtd/upcloud-python-api/archive/%s.tar.gz' % version,
     license='MIT',
+    python_requires='>=2.6,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
     install_requires=[
         'requests>=2.6.0',
         'six>=1.9.0'
