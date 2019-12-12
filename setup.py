c735ca20338e729f6f19ed098332a1aaebbb6c94
Ville Skyttä <ville.skytta@upcloud.com>
2019-12-12 14:28:10 +0200

    setup.py: move version to a variable

--- setup.py
+++ setup.py
@@ -2,16 +2,19 @@
 
 from setuptools import setup
 
+
+version = '0.4.5'
+
 setup(
     name='upcloud-api',
-    version='0.4.5',
+    version=version,
     description='UpCloud API Client',
     author='Elias Nygren',
     maintainer='Mika Lackman',
     maintainer_email='mika.lackman@upcloud.com',
     url='https://github.com/UpCloudLtd/upcloud-python-api',
     packages=['upcloud_api', 'upcloud_api.cloud_manager'],
-    download_url='https://github.com/UpCloudLtd/upcloud-python-api/archive/0.4.5.tar.gz',
+    download_url='https://github.com/UpCloudLtd/upcloud-python-api/archive/%s.tar.gz' % version,
     license='MIT',
     install_requires=[
         'requests>=2.6.0',
