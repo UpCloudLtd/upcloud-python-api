1104502570400989e28de579f9679be3b4f0818f
Turo Mikkonen <turo.mikkonen@upcloud.com>
2021-02-11 17:38:27 +0200

    Version 1.0.1

--- upcloud_api/__init__.py
+++ upcloud_api/__init__.py
@@ -8,7 +8,7 @@ from __future__ import unicode_literals
 from __future__ import absolute_import
 
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __author__ = 'Elias Nygren'
 __author_email__ = 'elias.nygren@upcloud.com'
 __maintainer__ = 'UpCloud'
